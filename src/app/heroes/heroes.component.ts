36ecdb025b6076cbb6c55e880f34d05d9e5e3b5c
Jesús Triano <core.jesust@gmail.com>
2020-03-16 22:06:54 +0100

    Añadido al servicio de mensajes cada selección de un Heroe

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './../hero';
 // import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,9 +15,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   /* La siguiente linea se añade internamente por la declaración de parametros del constructor.
   private heroService: HeroService;
+  Se inyecta también el singleton de clase MessageService 
   */
   // constructor de la clase
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   // constructor del componente, se usa para inicializarlo
   ngOnInit(): void {
@@ -30,6 +32,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero; // Aquí la referencia de hero se asignará a selectedHero
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
 }
