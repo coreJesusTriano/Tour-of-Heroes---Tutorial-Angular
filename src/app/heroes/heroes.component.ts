ace7aeda288806465c170fe6e424570344536c73
Jesús Triano <core.jesust@gmail.com>
2020-03-18 17:04:08 +0100

    Desacoplamos el detalle de los heroes del componente Heroes, de modo que al componente de detalles ya no se mostrará abajo sino en una vista aparte. Eliminamos de heroes.Component el código que ya no es necesario para este nuevo enfoque. Montamos los enlaces al detalle en la plantilla del heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './../hero';
-// import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,16 +9,10 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
   heroes: Hero[];
-  /* La siguiente linea se añade internamente por la declaración de parametros del constructor.
-  private heroService: HeroService;
-  Se inyecta también el singleton de clase MessageService 
-  */
-  // constructor de la clase
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-  // constructor del componente, se usa para inicializarlo
+  constructor(private heroService: HeroService) { }
+
   ngOnInit(): void {
     this.getHeroes();
   }
@@ -28,11 +20,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero; // Aquí la referencia de hero se asignará a selectedHero
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
 }
