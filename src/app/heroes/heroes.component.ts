4276c8796d9b46538693b46cf2b6394fb9b85aed
coreJesusTriano <core.jesust@gmail.com>
2020-04-19 23:03:37 +0200

    Agregada alta nuevo Heroe

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,4 +21,15 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      // When addHero() saves successfully
+      .subscribe(hero => {
+        this.heroes.push(hero);
+        // this callback add, pushing, the new hero to heroes's array
+      });
+  }
 }
